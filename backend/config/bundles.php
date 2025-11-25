aebff8276dcf851ab568d2cfc3afa327f1663cf4
Andrey Arzamaskov <arzamaskov@gmail.com>
2025-11-25 21:09:37 +0400

    feat(auth): добавлена JWT аутентификация с refresh token
    - Настроен Lexik JWT Authentication Bundle
    - Добавлен Gesdinet JWT Refresh Token Bundle
    - Настроены firewalls для login и API endpoints
    - Исправлен маппинг RefreshToken (убраны дублирующиеся поля)

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -6,6 +6,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
